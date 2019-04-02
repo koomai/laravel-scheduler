af21d3cc106c08898c25aa47335d2960c949c937
Sid Kumai <sidkumai@nbnco.com.au>
2019-04-02 17:00:51 +1100

    removed prompts

--- src/Console/Commands/ScheduleAddCommand.php
+++ src/Console/Commands/ScheduleAddCommand.php
@@ -2,11 +2,8 @@
 
 namespace Koomai\Scheduler\Console\Commands;
 
-use DateTimeZone;
-use Illuminate\Support\Facades\Artisan;
-use Koomai\Scheduler\Console\Commands\Traits\ValidatesInput;
-use Koomai\Scheduler\Constants\TaskType;
 use Koomai\Scheduler\Console\Commands\Traits\BuildsScheduledTasksTable;
+use Koomai\Scheduler\Console\Commands\Traits\ValidatesInput;
 use Koomai\Scheduler\ScheduledTask;
 
 class ScheduleAddCommand extends ScheduleCommand
@@ -27,7 +24,6 @@ class ScheduleAddCommand extends ScheduleCommand
                 {--environments= : Comma-separated list of environments the task should run in}
                 {--queue= : Queue name if scheduled job needs to run on a specific queue}
                 {--without-overlapping : Set this flag if the task should run without overlapping}
-                {--on-one-server : Set this flag if the task should run on one server only. Requires redis/memcached cache driver}
                 {--run-in-background : Set this flag if the task should run in the background}
                 {--in-maintenance-mode : Set this flag if the task should run even in maintenance mode}
                 {--output-path= : Add path to file where output should be sent to}
@@ -49,7 +45,6 @@ class ScheduleAddCommand extends ScheduleCommand
     private $queue;
     private $environments;
     private $withoutOverlapping;
-    private $onOneServer;
     private $inMaintenanceMode;
     private $runInBackground;
     private $outputPath;
@@ -57,28 +52,12 @@ class ScheduleAddCommand extends ScheduleCommand
     private $outputEmail;
 
     /**
-     * Execute the console command.
-     *
-     * @return mixed
-     * @throws \ReflectionException
-     * @throws \Koomai\Constants\InvalidConstantException
-     */
-    public function handle()
-    {
-        if ($this->option('type') && $this->option('task') && $this->option('cron')) {
-            return $this->handleWithoutPrompts();
-        }
-
-        return $this->handleWithPrompts();
-    }
-
-    /**
      * Execute the console command by parsing options
      *
      * @return mixed
      * @throws \Koomai\Constants\InvalidConstantException
      */
-    private function handleWithoutPrompts()
+    public function handle()
     {
         if (! $this->validate($this->options())) {
             foreach ($this->errors as $error) {
@@ -99,7 +78,6 @@ class ScheduleAddCommand extends ScheduleCommand
                 : explode(',', $this->option('environments'))
             );
         $this->withoutOverlapping = config('scheduler.without_overlapping') ?? $this->option('without-overlapping');
-        $this->onOneServer = config('scheduler.on_one_server') ?? $this->option('on-one-server');
         $this->inMaintenanceMode = config('scheduler.in_maintenance_mode') ?? $this->option('in-maintenance-mode');
         $this->runInBackground = config('scheduler.run_in_background') ?? $this->option('run-in-background');
         $this->outputPath = config('scheduler.output_path') ?? $this->option('output-path');
@@ -108,159 +86,6 @@ class ScheduleAddCommand extends ScheduleCommand
 
         $scheduledTask = $this->createTask();
         $this->generateTable($scheduledTask);
-    }
-
-    /**
-     * Execute the console command by prompting the user for options
-     *
-     * @return mixed
-     * @throws \ReflectionException
-     */
-    private function handleWithPrompts()
-    {
-        $this->type = $this->choice(trans('scheduler::questions.type'), TaskType::values());
-
-        $this->task = $this->askForTask();
-
-        if (!$this->task) {
-            $this->error(trans('scheduler::messages.invalid_task_type', ['type' => $this->type]));
-
-            return 1;
-        }
-
-        $this->taskDescription = $this->ask(trans('scheduler::questions.description'));
-        $this->cron = $this->askForCronExpression();
-        $this->timezone = $this->askForTimezone();
-        $this->environments = config('scheduler.environments') ?: $this->askForEnvironments();
-        $this->withoutOverlapping = config('scheduler.without_overlapping') ?? $this->askIfTaskShouldRunWithoutOverlapping();
-        $this->onOneServer = config('scheduler.on_one_server') ?? $this->askIfTaskShouldRunOnOneServer();
-        $this->inMaintenanceMode = config('scheduler.in_maintenance_mode') ?? $this->askIfTaskShouldRunInMaintenanceMode();
-        $this->runInBackground = config('scheduler.run_in_background') ?? $this->askIfTaskShouldRunInBackground();
-        $this->outputPath = config('scheduler.output_path') ?? $this->askForOutputFilePath();
-        $this->outputEmail = config('scheduler.output_email') ?? $this->askForOutputEmail();
-
-        $scheduledTask = $this->createTask();
-        $this->generateTable($scheduledTask);
-    }
-
-    private function askForTask(): ?string
-    {
-        switch ($this->type) {
-            case TaskType::COMMAND:
-                $task = $this->askForArtisanCommandTask();
-                break;
-            case TaskType::JOB:
-                $task = $this->askForJobTask();
-                break;
-            default:
-                $task = null;
-        }
-
-        return $task;
-    }
-
-    private function askForArtisanCommandTask(): string
-    {
-        $command = $this->anticipate(trans('scheduler::questions.task.artisan'), array_keys(Artisan::all()));
-
-        while (! $this->isValidArtisanCommand($command)) {
-            $this->warn(trans('scheduler::messages.invalid_artisan_command', ['command' => $command]));
-            $command = $this->anticipate(trans('scheduler::questions.task.artisan'), array_keys(Artisan::all()));
-        }
-
-        return $command;
-    }
-
-    private function askForJobTask(): string
-    {
-        $job = $this->ask(trans('scheduler::questions.task.job'));
-
-        while (! $this->isValidJob($job)) {
-            $this->warn(trans('scheduler::messages.invalid_job_class', ['job' => $job]));
-            $job = $this->ask(trans('scheduler::questions.task.job'));
-        }
-
-        $this->queue = $this->ask(trans('scheduler::questions.queue'));
-
-        return $job;
-    }
-
-    private function askForCronExpression(): string
-    {
-        $cron = $this->ask(trans('scheduler::questions.cron'));
-
-        while (! $this->isValidCronExpression($cron)) {
-            $this->warn(trans('scheduler::messages.invalid_cron_expression', ['cron' => $cron]));
-            $cron = $this->ask(trans('scheduler::questions.cron'));
-        }
-
-        return $cron;
-    }
-
-    private function askForTimezone(): ?string
-    {
-        $timezone = $this->anticipate(trans('scheduler::questions.timezone'), DateTimeZone::listIdentifiers());
-
-        while ($timezone !== null && ! $this->isValidTimezone($timezone)) {
-            $this->warn(trans('scheduler::messages.invalid_timezone', ['timezone' => $timezone]));
-            $timezone = $this->anticipate(trans('scheduler::questions.timezone'), DateTimeZone::listIdentifiers());
-        }
-
-        return $timezone;
-    }
-
-    private function askForEnvironments(): array
-    {
-        $environments = $this->ask(trans('scheduler::questions.environments'));
-
-        return $environments === null ? [] : explode(',', $environments);
-    }
-
-    private function askIfTaskShouldRunWithoutOverlapping(): bool
-    {
-        return $this->choice(trans('scheduler::questions.overlapping'), ['No', 'Yes']) === 'Yes';
-    }
-
-    private function askIfTaskShouldRunOnOneServer(): bool
-    {
-        $choice = $this->choice(trans('scheduler::questions.one_server'), ['No', 'Yes']) === 'Yes';
-
-        if ($choice) {
-            $this->warn(trans('scheduler::messages.cache_driver_alert'));
-        }
-
-        return $choice;
-    }
-
-    private function askIfTaskShouldRunInMaintenanceMode(): bool
-    {
-        return $this->choice(trans('scheduler::questions.maintenance'), ['No', 'Yes']) === 'Yes';
-    }
-
-    private function askIfTaskShouldRunInBackground(): bool
-    {
-        return $this->choice(trans('scheduler::questions.background'), ['No', 'Yes']) === 'Yes';
-    }
-
-    private function askForOutputFilePath(): ?string
-    {
-        if ($this->type !== TaskType::JOB && $this->confirm(trans('scheduler::questions.confirm_output_path'))) {
-            $outputFilePath = $this->ask(trans('scheduler::questions.output_path'));
-            $this->appendOutput = $this->choice(trans('scheduler::questions.append_output'), ['No', 'Yes']) === 'Yes';
-
-            return $outputFilePath;
-        }
-
-        return null;
-    }
-
-    private function askForOutputEmail(): ?string
-    {
-        if ($this->type !== TaskType::JOB) {
-            return $this->ask(trans('scheduler::questions.output_email'));
-        }
-
-        return null;
     }
 
     /**
@@ -278,7 +103,6 @@ class ScheduleAddCommand extends ScheduleCommand
                 'environments' => $this->environments,
                 'queue' => $this->queue,
                 'without_overlapping' => $this->withoutOverlapping,
-                'on_one_server' => $this->onOneServer,
                 'run_in_background' => $this->runInBackground,
                 'in_maintenance_mode' => $this->inMaintenanceMode,
                 'output_path' => $this->outputPath,
