64fb153cf98e2a6b93077c069a296cfbe67e8d80
Sid Kumai <sidkumai@nbnco.com.au>
2019-04-02 17:01:10 +1100

    updated language file for 5.5

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,9 +2,8 @@
 
 return [
     'invalid_task_type' => '[:type] is an invalid scheduled task type. Valid types are – command and job.',
-    'invalid_artisan_command' => '[:command] not a valid artisan command. Please try again or press Ctrl+C to exit.',
-    'invalid_job_class' => '[:job] class does not exist. Please try again or press Ctrl+C to exit.',
-    'invalid_cron_expression' => '[:cron] is an invalid cron expression. Please try again or press Ctrl+C to exit.',
-    'invalid_timezone' => '[:timezone] is an invalid timezone. Leave empty to use default timezone. Please try again or press Ctrl+C to exit.',
-    'cache_driver_alert' => 'Ensure your default cache driver is redis or memcached – https://laravel.com/docs/scheduling#running-tasks-on-one-server',
-];
+    'invalid_artisan_command' => '[:command] not a valid artisan command. Please try again. Use --help flag for more details.',
+    'invalid_job_class' => '[:job] class does not exist. Please try again.',
+    'invalid_cron_expression' => '[:cron] is an invalid cron expression. Please try again.',
+    'invalid_timezone' => '[:timezone] is an invalid timezone. Leave empty to use default timezone.',
+    ];
